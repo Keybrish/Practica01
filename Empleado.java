c001e3c577fa0497a57f7e5c5ce7a934954e424c
Keybrish Zamora <keychanpa@gmail.com>
2021-11-17 12:14:15 -0500

    Empleado modificado

---
+++ Empleado.java
@@ -0,0 +1,18 @@
+public class Empleado(){
+
+	private String cedula;
+	private String Nombre;
+
+	public String getCedula(){
+		return this.cedula;
+	}
+
+	public String getNombre(){
+		return this.nombre;
+	}
+
+	public void setNombre(String Nombre){
+		this.nombre = nombre:
+	}
+
+}
